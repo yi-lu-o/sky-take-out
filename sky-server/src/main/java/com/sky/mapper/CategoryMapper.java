7d1f0d31a989401b60b3f0e227b813f33b7c2318
一路 <148515341+yi-lu-o@users.noreply.github.com>
2023-10-31 09:43:53 +0800

    完成了公共字段自动填充功能开发

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.enumeration.OperationType;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
@@ -16,7 +17,8 @@ public interface CategoryMapper {
      * 插入数据
      * @param category
      */
-    @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+    @AutoFill(value = OperationType.INSERT)
+    @Insert("insert into sky_take_out.category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);
@@ -32,13 +34,14 @@ public interface CategoryMapper {
      * 根据id删除分类
      * @param id
      */
-    @Delete("delete from category where id = #{id}")
+    @Delete("delete from sky_take_out.category where id = #{id}")
     void deleteById(Long id);
 
     /**
      * 根据id修改分类
      * @param category
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Category category);
 
     /**
