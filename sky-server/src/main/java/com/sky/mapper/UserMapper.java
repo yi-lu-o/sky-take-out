04ea4dd247c81831c23c23e8f8831bc3fd21d360
一路 <148515341+yi-lu-o@users.noreply.github.com>
2023-11-06 14:46:06 +0800

    完成了用户统计功能开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -22,4 +24,10 @@ public interface UserMapper {
     void insert(User user);
     @Select("select * from sky_take_out.user where id = #{id}")
     User getById(Long id);
+    /**
+     * 根据动态条件统计用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
